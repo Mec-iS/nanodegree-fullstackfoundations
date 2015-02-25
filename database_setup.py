ddc53e9af4e9cb3b9b21a598844803dbea36a0de
Lorenzo <tunedconsulting@gmail.com>
2015-02-25 18:06:20 +0100

    First Lesson Done

---
+++ database_setup.py
@@ -0,0 +1,61 @@
+__author__ = 'lorenzo'
+
+'''Configuration'''
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+'''End Configuration'''
+
+'''Class'''
+class Restaurant(Base):
+    #
+    # Table
+    #
+    __tablename__ = 'restaurant'
+
+    #
+    # Mapper
+    #
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+    pass
+'''End Class'''
+
+'''Class'''
+class MenuItem(Base):
+    #
+    # Table
+    #
+    __tablename__ = 'menu_item'
+
+    #
+    # Mapper
+    #
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+    pass
+'''End Class'''
+
+'''Configuration'''
+engine = create_engine(
+    r'sqlite:///C:\Github\restaurantmenu.db'
+)
+'''End Configuration'''
+
+
+
---
+++ tests/db_test.py
@@ -0,0 +1,97 @@
+__author__ = 'lorenzo'
+
+
+from sqlalchemy.orm import sessionmaker, Query
+from database_setup import engine, Base, Restaurant, MenuItem
+
+
+def create_sqlite_file(eng):
+    Base.metadata.create_all(eng)
+
+
+def start_session(eng):
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=eng)
+    session = DBSession()
+    return session
+
+
+def insert_mock_data(ssn):
+    my_first_restaurant = Restaurant(name="Pizza Palace")
+    pizza_mozzarella = MenuItem(name="Pizza Mozzarella", description="Natural tomato sauce and mozzarella",
+                                course="Entree", price="8.99$", restaurant=my_first_restaurant)
+    ssn.add(pizza_mozzarella)
+    ssn.add(my_first_restaurant)
+    ssn.commit()
+
+
+def query_restaurants_all(ssn):
+    for q in ssn.query(Restaurant).all():
+        print q.name
+
+
+def query_filter_by(ssn, name=None, item_id=None):
+    if name:
+        return ssn.query(MenuItem).filter_by(name=name)
+    elif item_id:
+        return ssn.query(MenuItem).filter_by(id=item_id).one()
+    else:
+        raise AttributeError
+
+
+def return_info_from_items(query):
+    if isinstance(query, Query):
+        if query.count() == 1:
+            q = query
+            print q.id
+            print q.price
+            print q.restaurant.name
+            print "\n"
+        else:
+            for q in query:
+                print q.id
+                print q.price
+                print q.restaurant.name
+                print "\n"
+    else:
+        raise TypeError
+
+
+def update_item_price(ssn, query, new_price):
+    if query.count() == 1:
+        query.price = new_price
+        ssn.add(query)
+        return ssn.commit()
+    else:
+        query = query
+        for q in query:
+            q.price = new_price
+            ssn.add(q)
+        return ssn.commit()
+
+
+
+
+#create_sqlite_file(engine)
+session = start_session(engine)
+################
+#insert_mock_data(session)
+#query_restaurants_all(session)
+q = query_filter_by(session, name='Veggie Burger')
+return_info_from_items(q)
+#################
+
+#################
+#q1 = query_filter_by(session, item_id=10)
+#return_info_from_items(q1)
+#
+#update_item_price(session, q1, '2.99$')
+#
+#q1 = query_filter_by(session, item_id=10)
+#return_info_from_items(q1)
+#################
+
+#################
+#q2 = query_filter_by(session, name='Veggie Burger')
+#update_item_price(session, q2, '2.99$')
+#################
