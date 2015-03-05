aaaa70056e8df4328b5b738e066703137bea210a
Lorenzo <tunedconsulting@gmail.com>
2015-03-05 18:40:41 +0100

    LESSON 2 - Task 3

--- webserver.py
+++ webserver.py
@@ -14,6 +14,16 @@ def start_session(eng):
     return session
 
 
+def insert_restaurant(ssn, name):
+    """CREATE Restaurant"""
+    restaurant = Restaurant(name=name)
+    ssn.add(restaurant)
+    try:
+        ssn.commit()
+    except:
+        raise IOError
+
+
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
@@ -26,11 +36,29 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "<html><body>"
                 session = start_session(engine)
 
+                output += "<p><a href=\"/restaurants/new\">Make a new Restaurant</a></p>"
+
                 for q in session.query(Restaurant).all():
                     output += "<ul><li>" + q.name + "</li>"
                     output += "<li><a href=\"#\">Edit</a></li>"
                     output += "<li><a href=\"#\">Delete</a></li>"
                     output += "</ul><br/><br/>"
+
+                output += "</ul></body></html>"
+                session.close()
+                self.wfile.write(output)
+                return
+
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+
+                output += "<h2>New Restaurant's name: </h2>"
+                output += """<form method='POST' enctype='multipart/form-data' action='/restaurants/new'><input name="restaurant" type="text" ><input type="submit" value="Submit"> </form>"""
 
                 output += "</ul></body></html>"
                 self.wfile.write(output)
@@ -40,27 +68,34 @@ class webserverHandler(BaseHTTPRequestHandler):
             self.send_error(404, "File Not Found %s" % self.path)
 
     def do_POST(self):
-        try:
-            self.send_response(301)
-            self.end_headers()
+        if self.path.endswith("/restaurants/new"):
+            print "here"
+            try:
+                self.send_response(301)
+                self.end_headers()
 
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('restaurant')
+                    name = messagecontent[0]
+                    print name
 
-                output = ""
-                output += "<html><body>"
-                output += "<h2> Ok, how about this: </h2>"
-                output += "<h1> %s </h1>" % messagecontent[0]
+                    session = start_session(engine)
+                    try:
+                        insert_restaurant(session, name)
+                    except IOError as e:
+                        raise e
 
-                output += """<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>"""
-                output += "</html></body>"
-                self.wfile.write(output)
-                return
-
-        except:
-            pass
+                    output = ""
+                    output += "<html><body>"
+                    output += "New Restaurant %s inserted" % name
+                    output += "</ul></body></html>"
+                    session.close()
+                    self.wfile.write(output)
+                    return
+            except:
+                pass
 
 def main():
     try:
