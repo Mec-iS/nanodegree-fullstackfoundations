c572402a65e8662f52adf287e0bfdc3f95ed41d2
Lorenzo <tunedconsulting@gmail.com>
2015-03-05 19:39:10 +0100

    LESSON 2 - Task 4

--- webserver.py
+++ webserver.py
@@ -24,6 +24,30 @@ def insert_restaurant(ssn, name):
         raise IOError
 
 
+def query_filter_by(ssn, name=None, item_id=None):
+    """ Filters by name or by id if name is None"""
+    if name:
+        return ssn.query(Restaurant).filter_by(name=name)
+    elif item_id:
+        return ssn.query(Restaurant).filter_by(id=item_id).one()
+    else:
+        raise AttributeError
+
+
+def update_name(ssn, res_id, new_name):
+    """updates resource name filtering by id"""
+    try:
+        print  res_id
+        res = ssn.query(Restaurant).filter_by(id=res_id).one()
+        print res
+        res.name = new_name
+        print res.name
+        ssn.add(res)
+        return ssn.commit()
+    except:
+        raise IOError
+
+
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
@@ -40,7 +64,7 @@ class webserverHandler(BaseHTTPRequestHandler):
 
                 for q in session.query(Restaurant).all():
                     output += "<ul><li>" + q.name + "</li>"
-                    output += "<li><a href=\"#\">Edit</a></li>"
+                    output += "<li><a href=\"/restaurants/" + str(q.id) + "/edit\">Edit</a></li>"
                     output += "<li><a href=\"#\">Delete</a></li>"
                     output += "</ul><br/><br/>"
 
@@ -64,38 +88,91 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
 
+            if self.path.endswith("/edit"):
+                p = self.path
+                pos2 = p.rfind("/edit")
+                print p[13:pos2]
+                restaurant_id = p[13:pos2]
+
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                session = start_session(engine)
+
+                restaurant = query_filter_by(session, name=None, item_id=int(restaurant_id))
+
+                output += """<form method='POST' enctype='multipart/form-data' action='/restaurants/""" + str(restaurant.id) + """/edit'>
+                <input name="restaurant" type="text" placeholder='""" + restaurant.name + """'><input type="submit" value="Submit"> </form>"""
+
+                output += "</ul></body></html>"
+                session.close()
+                self.wfile.write(output)
+                return
+
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
     def do_POST(self):
-        if self.path.endswith("/restaurants/new"):
-            print "here"
-            try:
-                self.send_response(301)
-                self.end_headers()
-
+        try:
+            if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
                 if ctype == 'multipart/form-data':
                     fields = cgi.parse_multipart(self.rfile, pdict)
                     messagecontent = fields.get('restaurant')
                     name = messagecontent[0]
-                    print name
 
                     session = start_session(engine)
                     try:
                         insert_restaurant(session, name)
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        # self.send_header('Location', '/restaurants') # automatic redirect
+                        self.end_headers()
                     except IOError as e:
                         raise e
 
                     output = ""
                     output += "<html><body>"
                     output += "New Restaurant %s inserted" % name
+                    output += "<br><a href=\"/restaurants\">Back</a>"
                     output += "</ul></body></html>"
                     session.close()
                     self.wfile.write(output)
                     return
-            except:
-                pass
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('restaurant')
+                    new_name = messagecontent[0]
+
+                    p = self.path
+                    pos2 = p.rfind("/edit")
+
+                    restaurant_id = p[13:pos2]
+
+                    session = start_session(engine)
+
+                    try:
+                        update_name(session, res_id=int(restaurant_id), new_name=new_name)
+                    except IOError as e :
+                        raise e
+
+                    session.close()
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')  # automatic redirect
+                    self.end_headers()
+                    self.wfile.write("")
+                    return
+
+        except Exception:
+            pass
+
+
 
 def main():
     try:
